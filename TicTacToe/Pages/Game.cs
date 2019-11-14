c072469bf423e3ca582d329d8f4de74a7b93d1dd
Ethan Nguyen <etnguyen84@win.cis.ksu.edu>
2019-11-14 15:34:37 -0600

    adding checkers board and stuff

---
+++ TicTacToe/Pages/Game.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Pages
+{
+    public class Game
+    {
+        public Color CurrentTurn { get; set; }
+        public Checker[,] Board = new Checker[8, 8];
+        public bool GameOn { get; set; }
+
+        public Game()
+        {
+            GameOn = true;
+
+            for (int i =0; i < 8; i+=2)
+            {
+                //Black rows
+                Board[i, 0] = new Checker(Color.Black, i, 0);
+                Board[i + 1, 1] = new Checker(Color.Black, i + 1, 1);
+                Board[i, 2] = new Checker(Color.Black, i, 2);
+
+                Board[i+1, 5] = new Checker(Color.Red, i +1, 5);
+                Board[i, 6] = new Checker(Color.Red, i , 6);
+                Board[i+1, 7] = new Checker(Color.Red, i+1, 7);
+            }
+
+            CurrentTurn = Color.Red;
+        }
+
+        public string Serialize()
+        {
+            string state = GameOn.ToString() + "\n";
+            state += CurrentTurn.ToString() + "\n";
+
+            for (int y =0; y < 8; y++)
+            {
+                for (int x = 0; x <8; x++)
+                {
+                    if (Board[x,y] != null)
+                    {
+                        Checker checker = Board[x, y];
+                        state += $"{checker.Color}|{checker.King}|{checker.Coords.X}|{checker.Coords.Y}\n";
+                    }
+                }
+            }
+            return state;
+        }
+
+        /// <summary>
+        /// Deserializer
+        /// </summary>
+        /// <param name="state"></param>
+        /// <returns></returns>
+        public static Game Marshall(string state) 
+        {
+            string[] parts = state.Split("\n");
+            Game game = new Game();
+            game.GameOn = bool.Parse(parts[0]);
+            game.CurrentTurn = Enum.Parse<Color>(parts[1]);
+            //game.CurrentTurn = (Color)Enum.Parse(typeof(Color), parts[1]);
+            game.Board = new Checker[8, 8];
+
+            for (int i = 2; i < parts.Length-1; i++)
+            {
+                string[] pieces = parts[i].Split('|');
+                Color color = (Color)Enum.Parse(typeof(Color), pieces[0]);
+                bool king = bool.Parse(pieces[1]);
+                int x = int.Parse(pieces[2]);
+                int y = int.Parse(pieces[3]);
+
+                Checker checker = new Checker(color, x, y, king);
+                game.Board[x, y] = checker;
+            }
+
+            return game;
+            
+        }
+    }
+}
--- TicTacToe/wwwroot/js/site.js
+++ TicTacToe/wwwroot/js/site.js
@@ -2,3 +2,37 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+var selectedChecker;
+var checkers = document.getElementsByTagName("circle");
+
+function onClickChecker(event) {
+    if (selectedChecker) {
+        selectedChecker.setAttribute("stroke", "gray");
+    }
+        selectedChecker = event.target;
+        event.target.setAttribute("stroke", "green");
+}
+
+
+for (var i = 0; i < checkers.length; i++) {
+    checkers[i].addEventListener('click', onClickChecker);
+
+}
+
+var rects = document.getElementsByTagName("rect");
+
+function onClickSquare(event) {
+    if (selectedChecker) {
+        document.getElementById("cx").value = selectedChecker.getAttribute("data-x");
+        document.getElementById("cy").value = selectedChecker.getAttribute("data-y");
+        document.getElementById("sx").value = event.target.getAttribute("data-x");
+        document.getElementById("sy").value = event.target.getAttribute("data-y");
+        document.getElementById("move-form").submit();
+
+    }
+}
+
+for (var j = 0; j < rects.length; j++) {
+    rects[j].addEventListener('click', onClickSquare);
+}
